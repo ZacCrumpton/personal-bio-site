185f614814f9b3cca16bbb09b46bea867f23523b
Zac Crumpton <z.crumpton@gmail.com>
2020-01-28 22:46:47 -0600

    added additional styles

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const projects = [
     {
         title: "cool projects",
         screenshot: "https://i.pinimg.com/originals/dc/e2/cd/dce2cd398e7668eaa23262ddf6d3e069.jpg",
-        description: "super cool stuff",
-        techUsed: "html css javascript",
+        description: "In this project we had to do something spectacular invovling html css and javascript. The goal was to wow and impress, obviously, because its super cool",
+        techUsed: "<strong>This project invovled</strong>:html css javascript",
         url: "github",
         githubUrl: "https://github.com/ZacCrumpton",
         available: true,

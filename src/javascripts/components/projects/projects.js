5723c2cdf1eca6cbb9646876d0fc574f57dc0481
Zac Crumpton <z.crumpton@gmail.com>
2020-03-23 19:33:29 -0500

    wip

--- src/javascripts/components/projects/projects.js
+++ src/javascripts/components/projects/projects.js
@@ -1,22 +1,25 @@
 import utils from '../../helpers/utils';
 import projectData from '../../helpers/data/projectData';
 
+
 const buildProjectCard = () => {
-  let domString = '';
   const projectDisplay = projectData.getProjects();
-  if (projectDisplay.available === true) {
-    domString += '<article class="projBox">';
-    domString += `<h2 id="projTitle">${projectDisplay.title}</h2>`;
-    domString += `<img class="projectImages" src="${projectDisplay.screenshot}">`;
-    domString += '<section class="infoBox">';
-    domString += `<p class="paraInfo">${projectDisplay.description}</p>`;
-    domString += `<p class="paraInfo">${projectDisplay.techUsed}</p>`;
-    domString += `<p class="paraInfo"><a class="githubLinks" href="${projectDisplay.url}"><h3>Project 1</h3></a>`;
-    domString += `<p class="paraInfo"><a class="githubLinks" href="${projectDisplay.githubUrl}">Zac Crumpton - GitHub</a>`;
-    domString += '</section>';
-    domString += '</article>';
-  }
-  utils.printToDom('projectsPage', domString);
+  let domString = '';
+  projectDisplay.forEach((project) => {
+    if (project.available === true) {
+      domString += '<article class="projBox">';
+      domString += `<h2 id="projTitle">${project.title}</h2>`;
+      domString += `<img class="projectImages" src="${project.screenshot}">`;
+      domString += '<section class="infoBox">';
+      domString += `<p class="paraInfo">${project.description}</p>`;
+      domString += `<p class="paraInfo">${project.techUsed}</p>`;
+      domString += `<p class="paraInfo"><a class="githubLinks" href="${project.url}"><h3>Project 1</h3></a>`;
+      domString += `<p class="paraInfo"><a class="githubLinks" href="${project.githubUrl}">Zac Crumpton - GitHub</a>`;
+      domString += '</section>';
+      domString += '</article>';
+    }
+    utils.printToDom('projectsPage', domString);
+  });
 };
 
 

c0b8d7271f39db52d7bb6c18ccdb1449b0e7d3aa
Zac Crumpton <z.crumpton@gmail.com>
2020-04-06 21:42:51 -0500

    projects link to firebase

--- src/javascripts/components/projects/projects.js
+++ src/javascripts/components/projects/projects.js
@@ -1,28 +1,39 @@
 import utils from '../../helpers/utils';
 import projectData from '../../helpers/data/projectData';
 
-
-const buildProjectCard = () => {
-  const projectDisplay = projectData.getProjects();
+const buildProject = () => {
   let domString = '';
-  projectDisplay.forEach((project) => {
-    if (project.available === true) {
-      domString += '<div class="indivProjects">';
-      domString += '<article class="projBox">';
-      domString += `<h2 id="projTitle">${project.title}</h2>`;
-      domString += `<img class="projectImages" src="${project.screenshot}">`;
-      domString += '<section class="infoBox">';
-      domString += `<p class="paraInfo">${project.description}</p>`;
-      domString += `<p class="paraInfo">${project.techUsed}</p>`;
-      domString += `<p class="paraInfo"><a class="githubLinks" href="${project.url}"><h3>${project.title} github link</h3></a>`;
-      domString += `<p class="paraInfo"><a class="githubLinks" href="${project.githubUrl}">Zac Crumpton - GitHub</a>`;
-      domString += '</section>';
-      domString += '</article>';
-      domString += '</div>';
-    }
-    utils.printToDom('projectsPage', domString);
-  });
+  domString += '<div id="projectSection">';
+  projectData.getProjects()
+    .then((projects) => {
+      projects.forEach((projectInfo) => {
+        // eslint-disable-next-line no-use-before-define
+        domString += buildProjectCard(projectInfo);
+      });
+      utils.printToDom('projectsPage', domString);
+    })
+    .catch((err) => console.error('get projects failed', err));
 };
 
 
-export default { buildProjectCard };
+const buildProjectCard = (project) => {
+  let domString = '';
+  if (project.available === true) {
+    domString += '<div class="indivProjects">';
+    domString += '<article class="projBox">';
+    domString += `<h2 id="projTitle">${project.title}</h2>`;
+    domString += `<img class="projectImages" src="${project.screenshot}">`;
+    domString += '<section class="infoBox">';
+    domString += `<p class="paraInfo">${project.description}</p>`;
+    domString += `<p class="paraInfo">${project.techUsed}</p>`;
+    domString += `<p class="paraInfo"><a class="githubLinks" href="${project.url}"><h3>${project.title} github link</h3></a>`;
+    domString += `<p class="paraInfo"><a class="githubLinks" href="${project.githubUrl}">Zac Crumpton - GitHub</a>`;
+    domString += '</section>';
+    domString += '</article>';
+    domString += '</div>';
+  }
+  return domString;
+};
+
+
+export default { buildProject };
